08bc09b2382d014cd1efb67d82778649c6f59c77
Adithya Rosayru <107207994+arosayru@users.noreply.github.com>
2025-05-20 00:21:58 +0530

    Assets

---
+++ app/src/main/java/com/adithya/cinexperiencemovieapp/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.adithya.cinexperiencemovieapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Navigate to MainActivity after 3 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // prevent returning to splash
+        }, 3000)
+    }
+}
