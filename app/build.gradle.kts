1105909773192d9c165cda144f905a1923e0438b
Adithya Rosayru <107207994+arosayru@users.noreply.github.com>
2025-05-26 02:46:24 +0530

    stay fix search bar in movie detail page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("com.google.android.material:material:<latest_version>")
+
 
 }
